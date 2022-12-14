c1a590a6ffd2927eaf155ecdd5807d049c44919f
Jose G Martinez <jgermanmm@gmail.com>
2022-12-14 17:16:14 -0300

    PORT  10000

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const { PORT }= process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
+  server.listen(PORT || 10000, () => {
     console.log(`%s listening at ${PORT} ✈` ); // eslint-disable-line no-console
   });
 });
