69e72c16df20459071df44c3e3673444f0c75164
Jose G Martinez <jgermanmm@gmail.com>
2022-12-14 17:28:18 -0300

    arreglo env

--- api/index.js
+++ api/index.js
@@ -19,11 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const { PORT }= process.env;
+const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT || 10000, () => {
-    console.log(`%s listening at ${PORT} ✈` ); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`%s listening at ${port} ✈` ); // eslint-disable-line no-console
   });
 });
