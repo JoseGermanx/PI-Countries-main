89d8fbbd6eb7302705e6393caefe3c8b9258c785
Jose G Martinez <jgermanmm@gmail.com>
2022-12-22 21:32:07 -0300

    Habilitados datos para produccion

--- api/src/db.js
+++ api/src/db.js
@@ -9,12 +9,12 @@ const {
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false,
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false 
-  //   }
-  // }
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false 
+    }
+  }
    // lets Sequelize know we can use pg-native for ~30% more speed
 });
 const basename = path.basename(__filename);
