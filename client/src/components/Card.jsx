6173339c74e9ed6e398c96570489a7152bee5996
Jose G Martinez <jgermanmm@gmail.com>
2023-01-22 11:45:36 -0300

    Prueba de show act en hover

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 
 export default function Card({name, continente, flag, id}) {
   return (
-    <div className="card">
+    <div className="card" id="card">
       <div className="thumb"><img src={flag} alt="Bandera"/><div><h5>{continente}</h5></div></div>
-      <div className="text-card">
-        <div><h3>{name}</h3></div>
-        {/* <div><h5>Actividades: 5</h5></div> */}
+      <div className="text-card" id="text-card">
+        <div id="name"><h3>{name}</h3></div>
+        <div className="show-act"><h5>Actividades: 5</h5></div>
         <div className="ver-mas">
         <Link
             to={"/countries/" + id}>
