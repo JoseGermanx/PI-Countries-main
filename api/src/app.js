719c6e35d33d0e529be47bfc84287a529c822ae1
Jose G Martinez <jgermanmm@gmail.com>
2023-01-22 11:18:25 -0300

    Cambio de politica de control de acceso *

--- api/src/app.js
+++ api/src/app.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const allowOrigin = process.env.URLFECH || 'http://localhost:3000';
+const allowOrigin = process.env.URLFECH || '*';
 
 require('./db.js');
 
