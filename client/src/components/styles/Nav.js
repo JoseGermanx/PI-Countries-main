8d793bc5d94987af6279831d488a2f8113aea336
Jose G Martinez <jgermanmm@gmail.com>
2023-01-23 10:17:18 -0300

    navbar - fixed top

--- client/src/components/styles/Nav.js
+++ client/src/components/styles/Nav.js
@@ -8,7 +8,8 @@ export const Nav = styled.div`
   align-items: center;
   flex-wrap: wrap;
   background: white;
-  position: absolute;
+  position: fixed;
+  z-index: 1000;
   top: 0;
   left: 0;
   right: 0;
