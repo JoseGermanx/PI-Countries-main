af4c22ef990bd1afd8a742947e0474a1b6803fcb
Jose G Martinez <jgermanmm@gmail.com>
2023-02-09 12:55:28 -0300

    Condicional en user profile

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -3,14 +3,21 @@ import { useAuth0 } from '@auth0/auth0-react';
 import LogoutButton from './Logout';
 
 const UserProfile = () => {
-    const {user} = useAuth0();
+    const {user, isLoading, isAuthenticated} = useAuth0();
+    if (isLoading) {
+      return <div class="loader-wrapper">
+      <div class="loader"></div>
+    </div>;
+    }
   return (
+    isAuthenticated && (
     <div>
         <img src={user.picture} alt={user.name}/>
         <h2>{user.name}</h2>
         <p>{user.email}</p>
         <LogoutButton />
     </div>
+    )
   )
 }
 
