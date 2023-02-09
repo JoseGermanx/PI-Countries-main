ee1a3ea10d4c5b406369bc40b9af87f6812a6d1e
Jose G Martinez <jgermanmm@gmail.com>
2023-02-09 12:11:05 -0300

    redirectUri includo como parametro de authProvider

--- client/src/index.js
+++ client/src/index.js
@@ -14,6 +14,7 @@ ReactDOM.render(
   <Auth0Provider
     domain= {domain}
     clientId= {clienteId}
+    redirectUri={window.location.origin}
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
