66e97d523a5492bb7f5d7c13d35ba65af7d21679
Jose Germán Martínez <jgermanmm@gmail.com>
2023-03-09 14:42:18 -0300

    Borrado de variable de entorno no utilizada

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_, DB_SSL } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_ } = process.env;
 const sslOp = process.env.SSL_OPTIONS || false;
 
 const sequelize = new Sequelize(
